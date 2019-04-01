76d1ba088aa08a9da4136eae093a816726e6be63
Shea McKinney <sherakama@gmail.com>
2019-04-01 13:06:34 -0700

    Starting webpack builds.

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,115 +6,6 @@ module.exports = function(grunt) {
   // This is where we configure each task that we'd like to run.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    watch: {
-      // This is where we set up all the tasks we'd like grunt to watch for changes.
-      scripts: {
-        files: ['js/source/**/*.js'],
-        tasks: ['uglify'],
-        options: {
-          spawn: false,
-        },
-      },
-      images: {
-        files: ['images/source/*.{png,jpg,gif}'],
-        tasks: ['imagemin'],
-        options: {
-          spawn: false,
-        }
-      },
-      vector: {
-        files: ['images/source/**/*.svg'],
-        tasks: ['svgmin'],
-        options: {
-          spawn: false,
-        }
-      },
-      css: {
-        files: ['scss/**/*.scss'],
-        tasks: ['sass'],
-        options: {
-          interrupt: true
-        }
-      },
-      twig: {
-        files: ['templates/**/*.html.twig'],
-        tasks: ['uglify', 'svgmin', 'imagemin', 'sass']
-      }
-    },
-    uglify: {
-      // This is for minifying all of our scripts.
-      options: {
-        sourceMap: true,
-        mangle: false
-      },
-      my_target: {
-        files: [{
-          expand: true,
-          cwd: 'js/source',
-          src: '{,*/}*.js',
-          dest: 'js/build'
-        }]
-      }
-    },
-    imagemin: {
-      // This will optimize all of our images for the web.
-      dynamic: {
-        files: [{
-          expand: true,
-          cwd: 'img/source/',
-          src: ['{,*/}*.{png,jpg,gif}' ],
-          dest: 'img/optimized/'
-        }]
-      }
-    },
-    svgmin: {
-      options: {
-        plugins: [{
-          removeViewBox: false
-        }, {
-          removeUselessStrokeAndFill: false
-        }]
-      },
-      dist: {
-        files: [{
-          expand: true,
-          cwd: 'images/source/',
-          src: ['{,*/}*.svg' ],
-          dest: 'images/optimized/'
-        }]
-      }
-    },
-    sass: {
-      // This will compile all of our sass files
-      // Additional configuration options can be found at https://github.com/sindresorhus/grunt-sass
-      options: {
-        includePaths: [
-          "scss",
-          "node_modules/bourbon/core",
-          "node_modules/bourbon-neat/app/assets/stylesheets",
-          "node_modules/font-awesome/scss",
-          "node_modules/normalize.css",
-          "node_modules/neat-omega/core",
-          "node_modules",
-          "libraries/decanter/core/scss",
-          "libraries"
-        ],
-        sourceMap: true,
-        // This controls the compiled css and can be changed to nested, compact or compressed.
-        outputStyle: 'nested',
-        precision: 10
-      },
-      dist: {
-        files: {
-          'css/base/base.css':              'scss/base/index.scss',
-          'css/components/components.css':  'scss/components/index.scss',
-          'css/layout/layout.css':          'scss/layout/index.scss',
-          'css/states/states.css':          'scss/states/index.scss',
-          'css/theme/theme.css':            'scss/theme/index.scss',
-          'css/print/print.css':            'scss/print/index.scss'
-        }
-      }
-    },
     browserSync: {
       dev: {
         bsFiles: {
@@ -136,46 +27,12 @@ module.exports = function(grunt) {
         }
       }
     },
-    git_subtree_add: {
-      decanter: {
-        options: {
-          source: "git@github.com:SU-SWS/decanter.git",
-          branch: "master",
-          target: "libraries/decanter"
-        }
-      }
-    },
-    git_subtree_pull: {
-      decanter: {
-        options: {
-          source: "git@github.com:SU-SWS/decanter.git",
-          branch: "master",
-          target: "libraries/decanter"
-        }
-      }
-    },
-    git_subtree_push: {
-      decanter: {
-        options: {
-          source: "git@github.com:SU-SWS/decanter.git",
-          branch: "master",
-          target: "libraries/decanter"
-        }
-      }
-    },
     availabletasks: {
       tasks: {
         options: {
           filter: "include",
           tasks: [
-            'browserSync',
-            'imagemin',
-            'sass',
-            'svgmin',
-            'uglify',
-            'watch',
-            'devmode',
-            'decanter'
+            'browserSync'
           ]
         }
       }
@@ -183,19 +40,8 @@ module.exports = function(grunt) {
   });
 
   // This is where we tell Grunt we plan to use this plug-in.
-  grunt.loadNpmTasks('grunt-git');
-  grunt.loadNpmTasks('grunt-cmv-git-subtree');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
-  grunt.loadNpmTasks('grunt-svgmin');
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-browser-sync');
   grunt.loadNpmTasks('grunt-available-tasks');
-
-  // My tasks.
-  grunt.registerTask('devmode', "Watch and BrowserSync all in one.", ['browserSync', 'watch']);
-  grunt.registerTask('decanter', "Pull the latest from the decanter branch defined in gruntfile.js.", ['git_subtree_pull']);
 
   // This is where we tell Grunt what to do when we type "grunt" into the terminal.
   // Note: if you'd like to run and of the tasks individually you can do so by typing 'grunt mytaskname' alternatively
