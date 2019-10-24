3572003f60939fdb6a0197a1656188e0494379b9
Shea McKinney <sherakama@gmail.com>
2019-10-23 21:25:27 -0700

    fixup! Order is VERY important.

--- src/js/base.js
+++ src/js/base.js
@@ -3,15 +3,10 @@
  * A Webpack entry file for the theme.
  */
 
-// My config overrides.
-// This needs to be before the decanter.js so we can set the appropriate
-// variables like $su-image-path.
-import "../scss/config/index.scss";
+ // Include Decanter.
+ // Right now we import the components as the main decanter.js file always
+ // imports all of the SASS and we want to make changes to it before we render.
+ import "decanter/core/src/js/components/components.js";
 
-// Include Decanter.
-import "decanter/core/src/js/decanter.js";
-
-// My base styles.
-// It is important that this comes after the decanter.js as it is lower in the
-// css cascade and allows us to override Decanter core features.
-import "../scss/base/index.scss";
+ // MY Base SCSS styles.
+ import "../scss/base/index.scss";
