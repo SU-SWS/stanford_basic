370d55c716c5162a614dae3156baae31b6cc9788
Shea McKinney <sherakama@gmail.com>
2019-10-23 20:42:07 -0700

    fixup! Order is important.

--- src/js/base.js
+++ src/js/base.js
@@ -4,7 +4,14 @@
  */
 
 // My config overrides.
-import "../scss/base/index.scss";
+// This needs to be before the decanter.js so we can set the appropriate
+// variables like $su-image-path.
+import "../scss/config/index.scss";
 
-// Include decanter.
+// Include Decanter.
 import "decanter/core/src/js/decanter.js";
+
+// My base styles.
+// It is important that this comes after the decanter.js as it is lower in the
+// css cascade and allows us to override Decanter core features.
+import "../scss/base/index.scss";
