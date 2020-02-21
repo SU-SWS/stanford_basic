a75f3e7634c4c91a53f1dfbee7d30b7d692f4dc6
Sherakama <sherakama@gmail.com>
2020-02-21 08:31:35 -0800

    D8CORE-1352: Prevent javascript active path highlighting on secondary nav. (#135)

--- src/js/components/secondary-nav/secondary-nav-static.js
+++ src/js/components/secondary-nav/secondary-nav-static.js
@@ -3,7 +3,11 @@ document.addEventListener('DOMContentLoaded', event => {
   var secondaryNavs = document.querySelectorAll('.su-secondary-nav');
   secondaryNavs.forEach((nav, index) => {
     if (nav.className.match(/su-secondary-nav--static/)) {
-      new SecondaryNavStatic(nav);
+      var options = {
+        expand: false,
+        activeTrail: false
+      };
+      new SecondaryNavStatic(nav, options);
     }
   });
 });
