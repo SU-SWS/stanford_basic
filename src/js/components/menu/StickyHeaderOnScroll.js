5b188363cecb5f57b597686c4cdd8e9a0635dbb1
Rebecca Hong <Rebeccahongsf@gmail.com>
2020-05-04 11:59:34 -0700

    Rewrite conditional statement for checking toolbar height

--- src/js/components/menu/StickyHeaderOnScroll.js
+++ src/js/components/menu/StickyHeaderOnScroll.js
@@ -6,17 +6,16 @@ var header = document.getElementById('block-stanford-basic-local-tasks'),
   sticky = header.getBoundingClientRect().top;
 
 function stickyHeaderOnScroll() {
-  var toolbarHeight = document.getElementById('toolbar-administration')
-      .offsetHeight,
+  var toolbarHeight = 0,
     toolbarOpen = document.body.classList.contains('toolbar-tray-open');
-  console.log('Toolbar Status:' + toolbarOpen);
+  if (toolbarOpen === true) {
+    toolbarHeight = 79;
+  } else {
+    toolbarHeight = 39;
+  }
   if (window.pageYOffset >= sticky - toolbarHeight) {
     header.classList.add('sticky');
-    if (toolbarOpen === false) {
-      header.style.marginTop = '39px';
-    } else if (toolbarOpen === true) {
-      header.style.marginTop = '79px';
-    }
+    header.style.marginTop = toolbarHeight;
   } else {
     header.classList.remove('sticky');
     header.style.marginTop = '0';
