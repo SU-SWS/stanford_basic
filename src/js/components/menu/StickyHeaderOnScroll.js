c40d92717c20116b24c62a64f67ef8b16f9fcd13
Rebecca Hong <Rebeccahongsf@gmail.com>
2020-05-04 12:20:14 -0700

    Add pixel string

--- src/js/components/menu/StickyHeaderOnScroll.js
+++ src/js/components/menu/StickyHeaderOnScroll.js
@@ -8,16 +8,18 @@ var header = document.getElementById('block-stanford-basic-local-tasks'),
 function stickyHeaderOnScroll() {
   var toolbarHeight = 0,
     toolbarOpen = document.body.classList.contains('toolbar-tray-open');
+
   if (toolbarOpen === true) {
     toolbarHeight = 79;
   } else {
     toolbarHeight = 39;
   }
+
   if (window.pageYOffset >= sticky - toolbarHeight) {
     header.classList.add('sticky');
-    header.style.marginTop = toolbarHeight;
+    header.style.marginTop = toolbarHeight + 'px';
   } else {
     header.classList.remove('sticky');
-    header.style.marginTop = '0';
+    header.style.marginTop = '0px';
   }
 }
