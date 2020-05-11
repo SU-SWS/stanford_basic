675bdb4e1b77814898b5923c846b1ec0795d05fb
Rebecca Hong <Rebeccahongsf@gmail.com>
2020-05-11 09:10:23 -0700

    D8CORE-2004 Resolve CodeClimate issues

--- src/js/theme/menu/StickyHeaderOnScroll.js
+++ src/js/theme/menu/StickyHeaderOnScroll.js
@@ -2,28 +2,28 @@ if (document.body.classList.contains('role--authenticated')) {
   window.onscroll = function () {
     stickyHeaderOnScroll();
   };
-  
+
   var header = document.getElementById('block-stanford-basic-local-tasks');
   var sticky = header.getBoundingClientRect().top;
-}
 
-function stickyHeaderOnScroll() {
-  var toolbarHeight = 0;
-  var toolbarOpen = document.body.classList.contains('toolbar-tray-open');
+  function stickyHeaderOnScroll() {
+    var toolbarHeight = 0;
+    var toolbarOpen = document.body.classList.contains('toolbar-tray-open');
 
-  if (toolbarOpen === true) {
-    toolbarHeight = 79;
-  }
-  else {
-    toolbarHeight = 39;
-  }
+    if (toolbarOpen === true) {
+      toolbarHeight = 79;
+    }
+    else {
+      toolbarHeight = 39;
+    }
 
-  if (window.pageYOffset >= sticky - toolbarHeight) {
-    header.classList.add('sticky');
-    header.style.marginTop = toolbarHeight + 'px';
-  }
-  else {
-    header.classList.remove('sticky');
-    header.style.marginTop = '0px';
+    if (window.pageYOffset >= sticky - toolbarHeight) {
+      header.classList.add('sticky');
+      header.style.marginTop = toolbarHeight + 'px';
+    }
+    else {
+      header.classList.remove('sticky');
+      header.style.marginTop = '0px';
+    }
   }
 }
