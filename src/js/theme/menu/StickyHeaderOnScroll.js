a673ca0a6d9a12e8d414f1dec69f72c2f620c130
Rebecca Hong <Rebeccahongsf@gmail.com>
2020-05-11 09:37:10 -0700

    D8CORE-2004 Resolve CodeClimate issues

--- src/js/theme/menu/StickyHeaderOnScroll.js
+++ src/js/theme/menu/StickyHeaderOnScroll.js
@@ -1,27 +1,25 @@
-if (document.body.classList.contains('role--authenticated')) {
-  window.onscroll = function () {
-    stickyHeaderOnScroll();
-  };
+window.onscroll = function () {
+  stickyHeaderOnScroll();
+};
 
+function stickyHeaderOnScroll() {
+  var authenticated = document.body.classList.contains('role--authenticated');
   var header = document.getElementById('block-stanford-basic-local-tasks');
-  var sticky = header.getBoundingClientRect().top;
+  var toolbarHeight = 0;
+  var toolbarOpen = document.body.classList.contains('toolbar-tray-open');
 
-  function stickyHeaderOnScroll() {
-    var toolbarHeight = 0;
-    var toolbarOpen = document.body.classList.contains('toolbar-tray-open');
-
+  if (authenticated) {
+    var sticky = header.getBoundingClientRect().top;
     if (toolbarOpen === true) {
       toolbarHeight = 79;
-    }
-    else {
+    } else {
       toolbarHeight = 39;
     }
 
     if (window.pageYOffset >= sticky - toolbarHeight) {
       header.classList.add('sticky');
       header.style.marginTop = toolbarHeight + 'px';
-    }
-    else {
+    } else {
       header.classList.remove('sticky');
       header.style.marginTop = '0px';
     }
