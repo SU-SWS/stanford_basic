8fc336c4cd849a180d69ed8e852fb0843d3312f7
Rebecca Hong <Rebeccahongsf@gmail.com>
2020-05-11 09:53:59 -0700

    D8CORE-2004 Resolve CodeClimate issues

--- src/js/theme/menu/StickyHeaderOnScroll.js
+++ src/js/theme/menu/StickyHeaderOnScroll.js
@@ -12,14 +12,16 @@ function stickyHeaderOnScroll() {
     var sticky = header.getBoundingClientRect().top;
     if (toolbarOpen === true) {
       toolbarHeight = 79;
-    } else {
+    }
+    else {
       toolbarHeight = 39;
     }
 
     if (window.pageYOffset >= sticky - toolbarHeight) {
       header.classList.add('sticky');
       header.style.marginTop = toolbarHeight + 'px';
-    } else {
+    }
+    else {
       header.classList.remove('sticky');
       header.style.marginTop = '0px';
     }
