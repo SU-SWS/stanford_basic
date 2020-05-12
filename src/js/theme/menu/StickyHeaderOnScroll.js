bcdd339b11f8c1ea8409bca50f10c63f2e8607d6
Rebecca Hong <Rebeccahongsf@gmail.com>
2020-05-11 17:16:15 -0700

    D8CORE-2004 Rewrite conditional statement to check for local tabs. Move roll up into behavior.js

--- src/js/theme/menu/StickyHeaderOnScroll.js
+++ src/js/theme/menu/StickyHeaderOnScroll.js
@@ -1,29 +1,29 @@
-window.onscroll = function () {
-  stickyHeaderOnScroll();
-};
+var header = document.getElementById('block-stanford-basic-local-tasks');
+
+if (header) {
+  var sticky = header.getBoundingClientRect().top;
+  window.onscroll = function () {
+    stickyHeaderOnScroll();
+  };
+}
 
 function stickyHeaderOnScroll() {
-  var authenticated = document.body.classList.contains('role--authenticated');
-  var header = document.getElementById('block-stanford-basic-local-tasks');
   var toolbarHeight = 0;
   var toolbarOpen = document.body.classList.contains('toolbar-tray-open');
 
-  if (authenticated) {
-    var sticky = header.getBoundingClientRect().top;
-    if (toolbarOpen === true) {
-      toolbarHeight = 79;
-    }
-    else {
-      toolbarHeight = 39;
-    }
+  if (toolbarOpen === true) {
+    toolbarHeight = 79;
+  }
+  else {
+    toolbarHeight = 39;
+  }
 
-    if (window.pageYOffset >= sticky - toolbarHeight) {
-      header.classList.add('sticky');
-      header.style.marginTop = toolbarHeight + 'px';
-    }
-    else {
-      header.classList.remove('sticky');
-      header.style.marginTop = '0px';
-    }
+  if (window.pageYOffset >= sticky - toolbarHeight) {
+    header.classList.add('sticky');
+    header.style.marginTop = toolbarHeight + 'px';
+  }
+  else {
+    header.classList.remove('sticky');
+    header.style.marginTop = '0px';
   }
 }
