e229a4095b4d49358bfd0ea583028ac4f28b5e42
Rebecca Hong <Rebeccahongsf@gmail.com>
2020-05-12 11:38:18 -0700

    D8CORE-2004 Remove console log

--- src/js/theme/menu/LocalTaskToggle.js
+++ src/js/theme/menu/LocalTaskToggle.js
@@ -2,7 +2,6 @@ var localTab = document.getElementById('block-stanford-basic-local-tasks');
 var pageContent = document.getElementById('page-content');
 
 function localTaskToggle() {
-  console.log(pageContent);
   if (localTab) {
     pageContent.parentNode.classList.add('outline');
   }
